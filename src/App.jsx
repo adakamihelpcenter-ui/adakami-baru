cbe69aab2ed86ef4a6bd19dc673f7f6d25894230
Adakami Help Center <adakami.help.center@gmail.com>
2026-04-14 18:50:18 +0700

    Update WhatsApp link in App component

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Footer from './components/Footer';
 import WhatsAppFloat from './components/WhatsAppFloat';
 
 function App() {
-  const whatsappLink = "https://wa.me/628811558464?text=Hallo%20admin,%20saya%20mau%20bertanya";
+  const whatsappLink = "https://wa.me/6288973187761?text=Hallo%20admin,%20saya%20mau%20bertanya";
 
   useEffect(() => {
     document.addEventListener('contextmenu', e => e.preventDefault());
